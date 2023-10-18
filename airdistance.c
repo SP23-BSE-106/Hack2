afbc8cc8cb21779bc8d44a68f3d5117458d723ba
Your Name <munazzajaved@gmail.com>
2023-10-18 23:47:00 +0500

    resolved all issues

--- airdistance.c
+++ airdistance.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <math.h>
  #define EARTH_RADIUS 6371.0
- double degreesToRadius(doublr degrees){
-    radius degrees * M_PI/180.0;
+ double degreesToRadians(double degrees){
+    return degrees * M_PI/180.0;
  }
- double calculateDistance(double lt1,double lon1,double lat2,double lon2){
-    lat1= degreesToRadians;
-    lon1= degreesToRadians;
-    lat2= degreesToRadians;
-    lon2= degreesToRadians;
+ double calculateDistance(double lat1,double lon1,double lat2,double lon2){
+    lat1= degreesToRadians(lat1);
+    lon1= degreesToRadians(lon1);
+    lat2= degreesToRadians(lat2);
+    lon2= degreesToRadians(lon2);
 
     double dlat = lat2 -lat1;
     double dlon = lon2 -lon1;
-    double a= sin(dlat/2)*sin(dlat/2) + cos(lat1) * cos(lat2)*sin(dlon/2) * sin (dlon/2);
-    double c= 2* atan2(sqrt(a),sqrt(1-a));
+    double a= sin(dlat/2)*sin(dlat/2) + cos(lat1) * cos(lat2) *sin(dlon/2) * sin (dlon/2);
+    double c= 2 * atan2(sqrt(a),sqrt(1-a));
     return EARTH_RADIUS* c;
  }
  int main()
@@ -31,7 +31,7 @@
     scanf("%lf",&lat2);
     printf("Longitude:");
     scanf("%lf",&lon2);
-    double distance = calculateDistance;
+    double distance = calculateDistance(lat1,lon1,lat2,lon2);
     printf("Air distance is %.6lf kms\n", distance);
     return 0;
  }
